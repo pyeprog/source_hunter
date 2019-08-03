823587d362e33334e53e6cd12449eb67d4ca5834
lwl <lwl@xkool.xyz>
2019-08-04 00:57:18 +0800

    refactoring parser

--- source_hunter/models/fnode.py
+++ source_hunter/models/fnode.py
@@ -10,12 +10,13 @@ class FNode:
         self.id = uuid4()
         self.file_path = file_path
         self.dir_path = os.path.dirname(file_path)
-        self.parser = ParserSelector.get_parser(self.file_path.split(".")[-1])
         self.content_lines = self.load(self.file_path)
         self.content_str = "\n".join(self.content_lines)
+        self._parser_class = ParserSelector.get_parser(self.file_path.split(".")[-1])
+        self._parser = self._parser_class(self.content_str)
         self.parent_node_set = set()
         self.children_node_set = set()
-        self.children_modules = self.parse_children_modules(self.content_lines)
+        self.children_modules = self.parse_children_modules()
 
     @staticmethod
     def load(file_path):
@@ -34,14 +35,18 @@ class FNode:
     def children(self):
         return list(self.children_node_set)
 
+    @property
+    def parser(self):
+        return self._parser
+
     def add_parent(self, node):
         self.parent_node_set.add(node)
 
     def add_child(self, node):
         self.children_node_set.add(node)
 
-    def parse_children_modules(self, content_list: List[str]):
-        return self.parser.parse_children_modules(content_list)
+    def parse_children_modules(self):
+        return self._parser.parse_children_modules()
 
     def get_calling_func_or_class(self, child_fnode, child_class_or_func: str):
-        return self.parser.get_calling_func_or_class(self, child_fnode, child_class_or_func)
+        return child_fnode.parser.get_calling_func_or_class(self.content_str, child_class_or_func)
--- source_hunter/parser.py
+++ source_hunter/parser.py
@@ -5,22 +5,22 @@ from source_hunter.utils.log_utils import logger
 
 
 class BaseParser(ABC):
-    def __init__(self, lang="python"):
-        self.lang = lang
+    @abstractmethod
+    def __init__(self, code_str: str):
+        raise NotImplementedError("This method is not implemented")
 
     @abstractmethod
-    def parse_children_modules(content_lines: List[str]):
+    def parse_children_modules(self):
         """
-        :param content_lines: list of str, lines of code
         :return: list of str, those statement that is responsible for import child module
         """
         raise NotImplementedError("This method is not implemented")
 
     @abstractmethod
-    def get_calling_func_or_class(parent_fnode, child_fnode, child_class_or_func: str):
+    def get_calling_func_or_class(self, parent_code_str: str, child_class_or_func: str):
         """
         check whether there is a func or class of parent_fnode calling the target child_class_or_func of child_fnode
-        :param child_fnode: fnode
+        :param parent_code_str: str, code string of parent fnode
         :param child_class_or_func: str, target class or function name
         :return: list of str, function or class names that calling the child_class_or_func
         """
@@ -28,57 +28,47 @@ class BaseParser(ABC):
 
 
 class PythonParser(BaseParser):
-    @staticmethod
-    def parse_children_modules(content_lines: List[str]):
-        import_patterns = []
-        import_patterns.append(re.compile("import (.*)"))
-        import_patterns.append(re.compile("from (.*) import (.*)"))
+    def __init__(self, code_str: str):
+        self.code_str = code_str
+        # TODO: add a code structure parsing method
+
+    def parse_children_modules(self):
         modules = []
-        for line in content_lines:
-            for pattern in import_patterns:
-                match = re.match(pattern, line)
-                if match:
-                    groups = match.groups()
-                    if len(groups) > 1:  # pattern: from abc import Abc
-                        for module_name in groups[1].split(","):
-                            modules.append(".".join([groups[0], module_name.strip()]))
-                    else:  # pattern: import Abc
-                        modules.append(groups[0])
+        # parse import in 'from example import ...' form
+        import_pattern = re.compile('from ([\w.]+) import (\([\w, \n]+\)|[\w_, ]+\n)')
+        matches = re.findall(import_pattern, self.code_str)
+        for match in matches:
+            pathes = match[0].strip().split('.')
+            for mod in match[1].strip().replace('\n', '').split(','):
+                if 'as' in mod:
+                    mod = mod.split('as')[0]
+                single_mod = pathes + [mod.strip()]
+                modules.append('.'.join(single_mod))
+
+        # parse import in 'import ...' form
+        import_pattern = re.compile('[^ \w_]import ([\w_, ]+)')
+        matches = re.findall(import_pattern, self.code_str)
+        for match in matches:
+            for single_mod in match.split(','):
+                if 'as' in single_mod:
+                    single_mod = single_mod.split('as')[0]
+                modules.append(single_mod.strip())
         return modules
 
-    @staticmethod
-    def get_calling_func_or_class(parent_fnode, child_fnode, child_class_or_func: str):
-        if child_fnode in parent_fnode.children_node_set:
-            func_pattern = re.compile("def (\w*?)\(.*?\):.*?{}".format(child_class_or_func), re.DOTALL)
-            func_matches = re.findall(func_pattern, parent_fnode.content_str)
-            logger.verbose_info(
-                'matching def (\w*?)\(.*?\):.*?{} in {}: {}'.format(child_class_or_func, parent_fnode.file_path,
-                                                                    func_matches))
-
-            class_pattern = re.compile("class (\w*?):.*?{}".format(child_class_or_func), re.DOTALL)
-            class_matches = re.findall(class_pattern, parent_fnode.content_str)
-            logger.verbose_info(
-                'matching class (\w*?):.*?{} in {}: {}'.format(child_class_or_func, parent_fnode.file_path,
-                                                                 class_matches))
-
-            matches = []
-            matches.extend(ma for ma in class_matches if ma)
-            matches.extend(ma for ma in func_matches if ma and ma != '__init__')
-            if matches:
-                logger.info(
-                    're.matching {} in {}: {}'.format(child_class_or_func, parent_fnode.file_path, matches))
-        return matches
+    def get_calling_func_or_class(self, parent_code_str: str, child_class_or_func: str):
+        # TODO: rectify this function
+        pass
 
 
 class ParserSelector:
     lang_parser_dict = {
-        "python":  PythonParser(),
-        "python3": PythonParser()
+        "python":  PythonParser,
+        "python3": PythonParser
     }
 
     suffix_parser_dict = {
-        ".py": PythonParser(),
-        "py":  PythonParser()
+        ".py": PythonParser,
+        "py":  PythonParser
     }
 
     @classmethod
